08945e61de2dec83d0d50595bc5605503d68e3e1
Joshua Lyons <joshua.lyons@cybirical.com>
2023-12-03 15:36:22 -0600

    Setup for day 4.

---
+++ Challenges/Challenge4.cs
@@ -0,0 +1,19 @@
+internal sealed class Challenge4 : IChallenge
+{
+    private readonly string[] _challengeData;
+
+    public Challenge4()
+    {
+        _challengeData = Utils.GetChallengeFileLines("4");
+    }
+
+    public void HelpSantaPartOne()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void HelpSantaPartTwo()
+    {
+        throw new NotImplementedException();
+    }
+}
